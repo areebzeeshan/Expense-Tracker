6ac7007f47d00b6f5790a7e3efa77bf608bc7b9e
Areeb Zeeshan <88983748+areebzeeshan@users.noreply.github.com>
2024-09-08 02:03:05 +0500

    Update (ExpenseListtable.jsx): apply text-center to the table

--- app/(routes)/dashboard/expenses/_components/ExpenseListtable.jsx
+++ app/(routes)/dashboard/expenses/_components/ExpenseListtable.jsx
@@ -22,17 +22,17 @@ const ExpenseListtable = ({ expensesList, refreshData }) => {
         <div className='mt-3'>
             <h2 className='font-bold text-lg'>Latest Expenses</h2>
             <div className='grid grid-cols-4 bg-slate-200 p-2 mt-3'>
-                <h2 className="font-bold">Name</h2>
-                <h2 className="font-bold">Amount</h2>
-                <h2 className="font-bold">Craeted At</h2>
-                <h2 className="font-bold">Action</h2>
+                <h2 className="font-bold text-center">Name</h2>
+                <h2 className="font-bold text-center">Amount</h2>
+                <h2 className="font-bold text-center">Craeted At</h2>
+                <h2 className="font-bold text-center">Action</h2>
             </div>
             {expensesList.map((expense, index) => (
                 <div key={index} className='grid grid-cols-4 bg-slate-50 p-2'>
-                    <h2>{expense.name}</h2>
-                    <h2>{expense.amount}</h2>
-                    <h2>{expense.createdAt}</h2>
-                    <h2 className="text-center">
+                    <h2 className="mx-auto">{expense.name}</h2>
+                    <h2 className="mx-auto">{expense.amount}</h2>
+                    <h2 className="mx-auto">{expense.createdAt}</h2>
+                    <h2 className="mx-auto">
                         <Trash
                             className='text-red-500 cursor-pointer'
                             onClick={() => deleteExpense(expense)}
